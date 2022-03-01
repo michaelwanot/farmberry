f70bf8e05d69c33beb677e9e58b244f2f39ea9fd
Michael Wanot <michael.wanot@sourcesense.com>
2022-03-01 21:28:13 +0100

    allowed host settings

--- farmberry/settings.py
+++ farmberry/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.56', 'localhost']
 
 # Application definition
 
