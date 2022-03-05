1bcc877d308a57b57d98800ccc8e98689d101485
Michael Wanot <michael.wanot@sourcesense.com>
2022-03-05 01:49:01 +0100

    python and system path for wsgi configuration

--- farmberry/settings.py
+++ farmberry/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['192.168.43.56', 'localhost']
+ALLOWED_HOSTS = ['192.168.43.56', 'localhost', '127.0.0.1']
 
 # Application definition
 
--- farmberry/wsgi.py
+++ farmberry/wsgi.py
@@ -7,7 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+# add the hellodjango project path into the sys.path
+sys.path.append('/home/pi/farmberry/farmberry')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/pi/farmberry/env/lib/python3.6/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
